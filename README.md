2a6d489d0b5550f40f223d17709956ca02915f30
PoivronJaune <poivronjaune@gmail.com>
2023-05-02 09:42:06 -0400

    Multi-version update

--- README.md
+++ README.md
@@ -1,13 +1,32 @@
 # Pi Pico W - Setup files
-Raspberry Pi Pico W project to setup a wifi connection and launch the installation of the 'phew' package on the pico W.  
+Raspberry Pi Pico W script to setup a wifi connection and launch the installation of the 'phew' package on the pico W.  
 
 ## Installation
 clone this repository on your local drive  
-Use [Thonny](https://thonny.org/) to upload secrets.py and setup_phew.py to your micro-controller (Raspberry Pi Pico W)  
+Use [Thonny](https://thonny.org/) (or your prefered IDE) to upload secrets.py and setup_phew.py to your micro-controller (Raspberry Pi Pico W)  
   
 ## Execution  
 From Thorny update the secrets.py parameters with your specific Wifi credentials  
 Open the setup_phew.py file and run it  
   
 This will connect to the Web, and install the phew library on the lib folder of your 'pico W' storage  
-Note: the script imports and uses the upip package (might be depracated in future releases of micro-python)  
+
+## Backwards compatibility
+From verion 1.20 of micropython for Raspberry Pi Pico W the package manager is mip.install().  
+For version before 1.20 (1.19 or lower) the package manager was upip (a lightweight version pip usually used in python environmenets)  
+This script will try to detect the version installed and use the proper package installer.  
+
+## Micropython version
+[Micropython Latest Version](https://micropython.org/download/rp2-pico-w/)  
+An older v1.19 micropython is available in thie repository for testing purposes.  
+
+## How to reset you micropyhton  (windows version())
+- Unplugthe Pico W from computer  
+- Hold the BOOTSEL button on the micro-controller  
+- Plug the USB cable in the micro-controller, this should open the - windows file explorer  
+- Drag the desired *.uf2 file to the micro-controller, that's it  
+- Open Thonny to connect to the Pico W  
+- Go to the Thonny console  
+- import os
+- os.uname() should display an object that contains the version - identification
+
--- setup_phew.py
+++ setup_phew.py
@@ -1,8 +1,25 @@
+import os
 import network
+import sys
 import urequests
 import time
-import upip
 from secrets import ssid, password
+
+micropython_version = os.uname()[2]
+micropython_version = float(f"{micropython_version.split('.')[0]}.{micropython_version.split('.')[1]}")
+print(micropython_version)
+if micropython_version > 1.19:
+    import mip
+else:
+    import upip
+
+
+def intro_msg():
+    print(f'Welcome to PicoSetup Phew Install script')
+    print(f'Microcontroller implementation  : {sys.implementation}')
+    print(f'Microcontroller Wifi capabilites: {hasattr(network, "WLAN")}')
+    print(f'Microcontroller __name__........: {machine.__name__}')
+    
 
 def wifi_list():
     wlan = network.WLAN(network.STA_IF)
@@ -17,6 +34,7 @@ def wifi_connect(ssid=ssid, pw=password):
     wlan.active(True)
     
     wlan.connect(ssid, pw)
+    
     #print(f'\nConnecting to WiFi PoivronSlow...')
     
     # Wait for connection or fail error
@@ -28,25 +46,35 @@ def wifi_connect(ssid=ssid, pw=password):
         time.sleep(1)
         
     if wlan.status() != 3:
-        raise RuntimeError('Wifi connection failed, please try again...')
+        #raise RuntimeError('Wifi connection failed, please try again...')
         return None
 
     print(f'Wifi connection established on {ssid} [status:{wlan.isconnected()}]')
     return wlan.ifconfig()[0]
 
+
 if __name__ == '__main__':
-    print('Launched picosetup manually from IDE...')
-    print('Change secrets.py to use your specific wifi configuration')
-    print(f'Connecting to [ {ssid} ] using [ {password} ]')
+    intro_msg()
+    print(f'Change secrets.py to use your specific wifi configuration')
+    print(f'Trying to connect to [ {ssid} ] using [ {password} ]')
     print('==========================')
     wifi_list()
     print('==========================')      
-    wifi_connect()
-    test_url = 'http://ip.jsontest.com/'
-    response = urequests.get(test_url)
-    ip = response.json()
-    print(f'Internet IP: {ip}')
-    upip.install('micropython-phew')
+    ip = wifi_connect()
+    if ip is not None:
+        print(f'Pico IP Address: {ip}')
+        test_url = 'http://ip.jsontest.com/'
+        response = urequests.get(test_url)
+        wan_ip = response.json()['ip']
+        print(f'Internet IP    : {wan_ip}')
+        #upip.install('micropython-phew')
+        #mip.install('https://github.com/pimoroni/phew') https://pimoroni.github.io/micropython-lib/mip/main
+        if micropython_version > 1.19:
+            mip.install('github:pimoroni/phew')
+        else:
+            upip.install('micropython-phew')
+    else:
+        print('Unable to connect to Wifi, please activate access point to setup Wifi fro this device')
      
     
     
